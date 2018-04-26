ece5e75b5ca9a5e89f0480cda737f62bca52b6e3
Punnadit Trussat <punnadittr@gmail.com>
2018-04-26 14:52:08 +0700

    complete version

--- linked_lists.rb
+++ linked_lists.rb
@@ -4,6 +4,14 @@ class Node
     @value = value
     @next = nil
   end
+  
+  def value
+    @value
+  end
+
+  def next_node
+    @next
+  end
 end
 
 class LinkedList
@@ -19,20 +27,11 @@ class LinkedList
       @tail = temp_node
       @head = temp_node
       @@node_counter += 1
-      @the_head = temp_node
     else
       @tail.next = temp_node
       @tail = temp_node
       @@node_counter += 1
     end
-  end
-
-  def head
-    @the_head
-  end
-
-  def tail
-    @tail
   end
 
   def prepend(value)
@@ -48,15 +47,122 @@ class LinkedList
     end
   end
 
+  def head
+    return nil if @head.nil?
+    output = []
+    output << @head.value << @head.next.value
+  end
+
+  def tail
+    return nil if @tail.nil?
+    output = []
+    output << @tail.value << nil
+  end
+
+  def insert_at(index, value)
+    return 'OUT OF INDEX RANGE' if index > size
+    return prepend(value) if index == 0
+    return append(value) if index == size
+    index_counter = 0
+    temp_node = @head
+    new_node = Node.new(value)
+    while index_counter < index - 1
+      temp_node = temp_node.next
+      index_counter += 1
+    end
+    new_node.next = temp_node.next
+    temp_node.next = new_node
+    @@node_counter += 1
+  end
+
+  def remove_at(index)
+    return "NO MORE ITEMS TO DELETE" if size == 0
+    return "OUT OF INDEX RANGE" if index > size - 1
+    return pop if index == (size - 1)
+    if index == 0
+      @head = @head.next
+      @@node_counter -= 1
+      return size
+    end
+    index_counter = 0
+    temp_node = @head
+    while index_counter < (index - 1)
+      temp_node = temp_node.next
+      index_counter += 1
+    end
+    temp_node.next = temp_node.next.next
+    @@node_counter -= 1
+  end
+
+  def at(index)
+    return head if index == 0
+    return tail if index == -1 || index + 1 == size
+    return [nil,nil] if index + 1 > size
+    node_output = []
+    index_counter = 0
+    temp_node = @head
+    while index_counter < index
+      temp_node = temp_node.next
+      index_counter += 1
+    end
+      node_output << temp_node.value << temp_node.next.value
+  end
+
+  def to_s
+    temp_node = @head
+    while temp_node != nil
+      print "(  #{temp_node.value}  ) -> "
+      temp_node = temp_node.next
+    end
+    print 'nil'
+  end
+
+  def contains?(value)
+    temp_node = @head
+    while temp_node != nil
+      return true if temp_node.value == value
+      temp_node = temp_node.next
+    end
+    return false
+  end
+
+  def find(value)
+    temp_node = @head
+    index_counter = 0
+    while temp_node != nil
+      return index_counter if temp_node.value == value
+      temp_node = temp_node.next
+      index_counter += 1
+    end
+  end
+
+  def pop
+    if size <= 1
+      @head = nil
+      @tail = nil
+      @@node_counter = 0
+    end
+    temp_node = @head
+    while temp_node != nil
+      if temp_node.next.next.nil?
+        temp_node.next = nil
+        @tail = temp_node
+        @@node_counter -= 1
+      end
+      temp_node = temp_node.next
+    end
+    size
+  end
+
   def size
     @@node_counter
   end
-
 end
 
 mylist = LinkedList.new
-mylist.prepend(0)
+mylist.append(0)
 mylist.append(1)
 mylist.append(2)
 mylist.append(3)
 mylist.append(4)
+mylist.append(5)
